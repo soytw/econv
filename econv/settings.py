ea1d3bc3e1505d7393258b83eb80da84ffe3b9b6
soytw <yo@soytw.noip.me>
2016-08-18 09:42:12 +0800

    setup index.html

--- econv/settings.py
+++ econv/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'econv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['econv/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
