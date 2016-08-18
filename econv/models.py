1f8948e516540ec454a5a8bda23255c0a8771bfc
soytw <yo@soytw.noip.me>
2016-08-18 12:58:00 +0800

    update codec list in database

--- econv/models.py
+++ econv/models.py
@@ -3,5 +3,5 @@ from django.db.models import CharField
 
 class Codec(Model):
 	name = CharField(max_length=20)
-	alias = CharField(max_length=100)
-	language = CharField(max_length=100)
+	aliases = CharField(max_length=100, blank=True, null=True)
+	languages = CharField(max_length=100, blank=True, null=True)
